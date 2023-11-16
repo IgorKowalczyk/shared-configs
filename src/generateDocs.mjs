a5bc47270c4bf7a01dcc6e97793b9a87633dc624
Igor Kowalczyk <majonezexe@protonmail.com>
2023-11-16 18:42:28 +0100

    Update generator script

--- src/generateDocs.mjs
+++ src/generateDocs.mjs
@@ -1,7 +1,56 @@
 import { markdownTable } from "markdown-table";
 import * as ESLintConfig from "../packages/eslint-config/flat.js";
 import * as PrettierConfig from "../packages/prettier-config/index.js";
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "node:fs";
+
+function capitalize(str) {
+ return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function generateESLintTable(config) {
+ return markdownTable([
+  ["Rule", "Style", "Type", "Documentation"],
+  ...Object.entries(config).map(([ruleName, ruleConfig]) => {
+   const ruleType = Array.isArray(ruleConfig) ? ruleConfig[0] : ruleConfig;
+   let description = Array.isArray(ruleConfig) ? ruleConfig[1] : ruleConfig;
+   const docs = ruleName.toString().includes("/") ? "External rule" : `[Documentation](https://eslint.org/docs/rules/${ruleName})`;
+
+   if (typeof description === "object") description = ruleName.toString().includes("/") ? "-" : docs;
+
+   const typeEmoji = {
+    error: "🚫",
+    warn: "⚠️",
+    off: "💡",
+   }[ruleType];
+
+   const typeText = {
+    error: "Error",
+    warn: "Warning",
+    off: "Disabled",
+   }[ruleType];
+
+   return [`\`${ruleName}\``, capitalize(description.toString()), `\`${typeEmoji} ${typeText}\``, docs];
+  }),
+ ]);
+}
+
+function generatePrettierTable(config) {
+ return markdownTable([
+  ["Rule", "Style", "Documentation"],
+  ...Object.entries(config).map(([ruleName, ruleConfig]) => {
+   const description = ruleName.toString().includes("/") ? "-" : `[Documentation](https://prettier.io/docs/en/options.html#${ruleName.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()})`;
+
+   return [`\`${ruleName}\``, `\`${ruleConfig}\``, description];
+  }),
+ ]);
+}
+
+function updateDocs(readmeContent, startMarker, endMarker, table) {
+ const start = readmeContent.slice(0, readmeContent.indexOf(startMarker) + startMarker.length);
+ const end = readmeContent.slice(readmeContent.indexOf(endMarker));
+
+ return `${start}\n${table}\n${end}`;
+}
 
 console.log("Generating documentation...");
 const time = Date.now();
@@ -10,61 +59,23 @@ const readmePath = "./README.md";
 const eslintPath = "./packages/eslint-config/README.md";
 const prettierPath = "./packages/prettier-config/README.md";
 
-const readmeContent = readFileSync(readmePath, "utf8");
-const eslintReadme = readFileSync(eslintPath, "utf8");
-const prettierReadme = readFileSync(prettierPath, "utf8");
-
 const startESLintMarker = "<!--START_SECTION:eslint-->";
 const endESLintMarker = "<!--END_SECTION:eslint-->";
-
 const startPrettierMarker = "<!--START_SECTION:prettier-->";
 const endPrettierMarker = "<!--END_SECTION:prettier-->";
 
-export function Capitalize(str) {
- return str.charAt(0).toUpperCase() + str.slice(1);
-}
+const globalReadmeContent = readFileSync(readmePath, "utf8");
+const eslintReadmeContent = readFileSync(eslintPath, "utf8");
+const prettierReadmeContent = readFileSync(prettierPath, "utf8");
 
-const ESLintTable = markdownTable([
- ["Rule", "Style", "Type", "Documentation"],
- ...Object.entries(ESLintConfig.default.rules).map(([ruleName, ruleConfig]) => {
-  const ruleType = Array.isArray(ruleConfig) ? ruleConfig[0] : ruleConfig;
-  let description = Array.isArray(ruleConfig) ? ruleConfig[1] : ruleConfig;
-  const docs = ruleName.toString().includes("/") ? "External rule" : `[Documentation](https://eslint.org/docs/rules/${ruleName})`;
+const ESLintTable = generateESLintTable(ESLintConfig.default.rules);
+const PrettierTable = generatePrettierTable(PrettierConfig.default);
 
-  if (typeof description === "object") {
-   description = ruleName.toString().includes("/") ? "-" : docs;
-  }
+const ESLintDocs = updateDocs(globalReadmeContent, startESLintMarker, endESLintMarker, ESLintTable);
+const PrettierDocs = updateDocs(ESLintDocs, startPrettierMarker, endPrettierMarker, PrettierTable);
 
-  const typeEmoji = {
-   error: "🚫",
-   warn: "⚠️",
-   off: "💡",
-  }[ruleType];
-
-  const typeText = {
-   error: "Error",
-   warn: "Warning",
-   off: "Disabled",
-  }[ruleType];
-
-  return [`\`${ruleName}\``, Capitalize(description.toString()), `\`${typeEmoji} ${typeText}\``, docs];
- }),
-]);
-
-const PrettierTable = markdownTable([
- ["Rule", "Style", "Documentation"],
- ...Object.entries(PrettierConfig.default).map(([ruleName, ruleConfig]) => {
-  const description = ruleName.toString().includes("/") ? "-" : `[Documentation](https://prettier.io/docs/en/options.html#${ruleName.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()})`;
-
-  return [`\`${ruleName}\``, `\`${ruleConfig}\``, description];
- }),
-]);
-
-const ESLintDocs = readmeContent.slice(0, readmeContent.indexOf(startESLintMarker) + startESLintMarker.length) + "\n" + ESLintTable + "\n" + readmeContent.slice(readmeContent.indexOf(endESLintMarker));
-const PrettierDocs = ESLintDocs.slice(0, ESLintDocs.indexOf(startPrettierMarker) + startPrettierMarker.length) + "\n" + PrettierTable + "\n" + ESLintDocs.slice(ESLintDocs.indexOf(endPrettierMarker));
-
-const ESLintPackageDocs = eslintReadme.slice(0, eslintReadme.indexOf(startESLintMarker) + startESLintMarker.length) + "\n" + ESLintTable + "\n" + eslintReadme.slice(eslintReadme.indexOf(endESLintMarker));
-const PrettierPackageDocs = prettierReadme.slice(0, prettierReadme.indexOf(startPrettierMarker) + startPrettierMarker.length) + "\n" + PrettierTable + "\n" + prettierReadme.slice(prettierReadme.indexOf(endPrettierMarker));
+const ESLintPackageDocs = updateDocs(eslintReadmeContent, startESLintMarker, endESLintMarker, ESLintTable);
+const PrettierPackageDocs = updateDocs(prettierReadmeContent, startPrettierMarker, endPrettierMarker, PrettierTable);
 
 writeFileSync(eslintPath, ESLintPackageDocs);
 writeFileSync(prettierPath, PrettierPackageDocs);
