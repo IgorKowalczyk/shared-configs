e8414731c3c440977774279daa3496ab59e01cf1
Igor Kowalczyk <majonezexe@protonmail.com>
2024-02-27 23:46:39 +0100

    Remove unused import and languageOptions in node.js config

--- packages/eslint-config/configs/flat/node.js
+++ packages/eslint-config/configs/flat/node.js
@@ -1,16 +1,10 @@
 import nodePluginRecommendedConfig from "eslint-plugin-n";
-import globals from "globals";
 
 export default Object.freeze([
  nodePluginRecommendedConfig.configs["flat/recommended"],
  {
   rules: {
    "n/shebang": "off",
-  },
-  languageOptions: {
-   globals: {
-    ...globals.node,
-   },
   },
  },
 ]);
