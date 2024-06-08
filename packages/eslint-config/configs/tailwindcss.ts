2780feb6b54b2dc023f064b3eab37f7154dc421e
Igor Kowalczyk <majonezexe@protonmail.com>
2024-06-08 14:01:15 +0200

    chore: Update eslint-config for tailwindcss

--- packages/eslint-config/configs/tailwindcss.ts
+++ packages/eslint-config/configs/tailwindcss.ts
@@ -1,10 +1,7 @@
-import { type Linter } from "eslint";
+import { composer } from "eslint-flat-config-utils";
 /* @ts-expect-error-next-line Waiting for types to be updated */
 import tailwindPlugin from "eslint-plugin-tailwindcss";
 
-export default [
- {
-  name: "@igorkowalczyk/eslint-config/tailwindcss",
-  ...tailwindPlugin.configs["flat/recommended"],
- },
-] as Linter.FlatConfig[];
+export default await composer(tailwindPlugin.configs["flat/recommended"]).override("tailwindcss:base", {
+ name: "@igorkowalczyk/eslint-config/tailwindcss",
+});
