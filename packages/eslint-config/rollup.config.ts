0ac28219bbb96f96d393b4ebee2f77945a9a722c
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-05 23:49:22 +0100

    Simplify build process, include types in the build

--- packages/eslint-config/rollup.config.ts
+++ packages/eslint-config/rollup.config.ts
@@ -1,20 +1,16 @@
-import { globSync } from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import rollupPluginCommonjs from "@rollup/plugin-commonjs";
 import rollupPluginNodeResolve from "@rollup/plugin-node-resolve";
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import { RollupOptions } from "rollup";
 import rollupPluginAutoExternal from "rollup-plugin-auto-external";
 
-const configFiles = globSync("configs/**/*.ts");
-const files = Object.fromEntries(configFiles.map((file) => [path.relative("configs", file.slice(0, file.length - path.extname(file).length)), fileURLToPath(new URL(file, import.meta.url))]));
-
 const defaultConfig = {
  output: {
   sourcemap: false,
   exports: "default",
   dir: "dist",
+  preserveModules: true,
+  preserveModulesRoot: "*",
  },
  treeshake: {
   annotations: true,
@@ -35,44 +31,19 @@ const defaultConfig = {
   }),
  ],
 
- // Due to the way the eslint-plugin-prettier/recommended config is structured, it needs to be included in the external array
  external: ["eslint-plugin-prettier/recommended"],
 } satisfies Partial<RollupOptions>;
 
 const config = [
  {
   ...defaultConfig,
-  output: [
-   {
-    ...defaultConfig.output,
-    entryFileNames: "configs/[name].js",
-    format: "esm",
-   },
-
-   // Uncomment the following block if you need CommonJS output as well
-   // {
-   //  ...defaultConfig.output,
-   //  entryFileNames: "configs/[name].cjs",
-   //  format: "cjs",
-   // },
-  ],
-  input: files,
- },
- {
-  ...defaultConfig,
   external: [
    // prettier
-   "./configs/astro",
-   "./configs/base",
-   "./configs/next",
-   "./configs/node",
-   "./configs/prettier",
-   "./configs/react",
-   "./configs/typescript",
+   ...defaultConfig.external,
   ],
   output: {
    ...defaultConfig.output,
-   entryFileNames: "index.js",
+   entryFileNames: "[name].js",
    format: "esm",
   },
   input: "index.ts",
