252e0fc6b682ddce7c9e0f1d6c8625e7ebb2cc8d
Igor Kowalczyk <majonezexe@protonmail.com>
2025-11-09 14:18:06 +0100

    Fix build

--- packages/eslint-config/rollup.config.ts
+++ packages/eslint-config/rollup.config.ts
@@ -3,51 +3,42 @@ import rollupPluginNodeResolve from "@rollup/plugin-node-resolve";
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import { RollupOptions } from "rollup";
 import rollupPluginAutoExternal from "rollup-plugin-auto-external";
-
-const defaultConfig = {
- output: {
-  sourcemap: false,
-  exports: "auto",
-  dir: "dist",
-  preserveModules: true,
-  preserveModulesRoot: ".",
- },
- treeshake: {
-  annotations: true,
-  moduleSideEffects: [],
-  propertyReadSideEffects: false,
-  unknownGlobalSideEffects: false,
- },
- plugins: [
-  rollupPluginAutoExternal(),
-  rollupPluginNodeResolve(),
-  rollupPluginCommonjs(),
-  rollupPluginTypescript({
-   tsconfig: "tsconfig.json",
-   include: ["configs/**/*.ts", "index.ts"],
-   declaration: true,
-   declarationDir: "dist",
-   outDir: "dist",
-  }),
- ],
-
- external: ["eslint-plugin-prettier/recommended", "eslint-config-next/core-web-vitals"],
-} satisfies Partial<RollupOptions>;
+import { globby } from "globby";
 
 const config = [
  {
-  ...defaultConfig,
-  external: [
-   // prettier
-   ...defaultConfig.external,
+  treeshake: {
+   annotations: true,
+   moduleSideEffects: [],
+   propertyReadSideEffects: false,
+   unknownGlobalSideEffects: false,
+  },
+  plugins: [
+   rollupPluginAutoExternal(),
+   rollupPluginNodeResolve(),
+   rollupPluginCommonjs(),
+   rollupPluginTypescript({
+    tsconfig: "tsconfig.json",
+    include: await globby(["configs/**/*.ts"]),
+    declaration: true,
+    rootDir: ".",
+    declarationDir: "dist",
+    outDir: "dist",
+   }),
   ],
+  input: await globby("configs/**/*.ts"),
   output: {
-   ...defaultConfig.output,
+   sourcemap: false,
+   exports: "auto",
+   dir: "dist",
+   preserveModules: true,
+   preserveModulesRoot: ".",
    entryFileNames: "[name].js",
    format: "esm",
   },
-  input: "index.ts",
+
+  external: ["eslint-plugin-prettier/recommended", "eslint-config-next/core-web-vitals"],
  },
-];
+] satisfies RollupOptions[];
 
 export default config;
